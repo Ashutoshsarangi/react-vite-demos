3971794ce0745abda0e14dd2e414e419310d5cf9
Ashu <ashutoshsarangi95@gmail.com>
2024-02-25 10:27:58 +0200

    Blog_Appwrite

---
+++ Vlog_AppWrite/src/App.jsx
@@ -0,0 +1,13 @@
+import { config } from "./config/config";
+import "./App.css";
+
+function App() {
+  console.log("Config ---> ", config);
+  return (
+    <>
+      <h1>This is Vlog Application With AppWrite</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ Vlog_AppWrite/src/config/config.js
@@ -0,0 +1,7 @@
+export const config = {
+  APPWRITE_ENDPOINT: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  PROJECT_ID: import.meta.env.VITE_PROJECT_ID,
+  DATABASE_ID: import.meta.env.VITE_DATABASE_ID,
+  COLLECTION_ID: import.meta.env.VITE_COLLECTION_ID,
+  BUCKET_ID: import.meta.env.VITE_BUCKET_ID,
+};
